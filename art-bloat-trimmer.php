4109a0e7bae37473b475bc64f83caa2163073104
Артем <artikus.sol@gmail.com>
2025-06-22 23:57:03 +0400

    fix(abt 2.1.0): readme & version

---
+++ art-bloat-trimmer.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Plugin Name: Art Bloat Trimmer
+ * Plugin URI: wpruse.ru
+ * Text Domain: art-bloat-trimmer
+ * Domain Path: /languages
+ * Description: Cleans WP code from unnecessary garbage and more!
+ * Version: 2.1.0
+ * Author: Artem Abramovich
+ * Author URI: https://wpruse.ru/
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ * WC requires at least: 5.2.0
+ * WC tested up to: 6.1
+ *
+ * RequiresWP: 5.5
+ * RequiresPHP: 8.0
+ *
+ * Copyright Artem Abramovich
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+const ABT_PLUGIN_DIR   = __DIR__;
+const ABT_PLUGIN_AFILE = __FILE__;
+const ABT_PLUGIN_VER   = '2.1.0';
+const ABT_PLUGIN_NAME  = 'Bloat Trimmer';
+const ABT_PLUGIN_SLUG  = 'art-bloat-trimmer';
+const ABT_PLUGIN_PREFIX  = 'abt';
+
+define( 'ABT_PLUGIN_URI', untrailingslashit( plugin_dir_url( ABT_PLUGIN_AFILE ) ) );
+define( 'ABT_PLUGIN_FILE', plugin_basename( __FILE__ ) );
+
+require ABT_PLUGIN_DIR . '/vendor/autoload.php';
+
+( new Art\BloatTrimmer\Main() )->init();
